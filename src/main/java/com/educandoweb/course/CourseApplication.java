15053bdb75e45c27b1b35a158bb40ae84b9442c4
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-22 11:11:36 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -3,11 +3,13 @@ package com.educandoweb.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.educandoweb.course.repositories.UserRepository;
+
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);
+		SpringApplication.run(CourseApplication.class, args);	
 	}
 
 }
