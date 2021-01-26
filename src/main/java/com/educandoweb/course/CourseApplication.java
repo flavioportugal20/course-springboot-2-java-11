65e18407d06db0d0ecb50c24e37d3169ec66f16a
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-25 21:48:30 -0300

    Prod profile

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -3,8 +3,6 @@ package com.educandoweb.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.educandoweb.course.repositories.UserRepository;
-
 @SpringBootApplication
 public class CourseApplication {
 
